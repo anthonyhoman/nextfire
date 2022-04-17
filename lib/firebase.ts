7c42ef47274c3ac768c78fdc58f57b19a705636b
Anthony Homan <ahoman@gmail.com>
2022-04-16 19:05:13 -0700

    08-step-completed

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,21 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
+
 const firebaseConfig = {
-    test: "test",
+    apiKey: "AIzaSyAm6AN0I2i00wsK9647x6tTNDxcvSSSg5A",
+    authDomain: "nextfire-ah.firebaseapp.com",
+    projectId: "nextfire-ah",
+    storageBucket: "nextfire-ah.appspot.com",
+    messagingSenderId: "1065663853030",
+    appId: "1:1065663853030:web:e3021c6b83ae041582e627"
 }
 
-export {}
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
