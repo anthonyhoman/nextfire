3ce9073af7874756e9f075e9bca73164a66196f9
Anthony Homan <ahoman@gmail.com>
2022-04-17 09:50:12 -0700

    12-step-complete

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import toast from 'react-hot-toast'
 
 import Loader from "../components/Loader"
 
 export default function Home() {
   return (
-    <div>
-      <Loader show />
-    </div>
+    <>
+      <div>
+        <Loader show />
+      </div>
+      <button onClick={() => toast.success("TOAST!")}>Toast Me</button>
+    </>
+
   )
 }
