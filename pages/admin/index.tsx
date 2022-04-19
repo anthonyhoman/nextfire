988649006b54539f149f724ba73e16bbed1a87a1
Anthony Homan <ahoman@gmail.com>
2022-04-19 14:32:07 -0700

    26-step-complete

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,11 @@
+import AuthCheck from "../../components/AuthCheck";
+
 export default function AdminPostsPage({ }) {
   return (
-    <main>Admin
+    <main>
+      <AuthCheck>
+        Admin Page
+      </AuthCheck>
     </main>
   )
 }
